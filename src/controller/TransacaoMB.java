2db9562dea995fafe4691aa6e5ce02e22ef6795a
Hugo Marques <Hugo Marques@DESKTOP-DG5VAED>
2017-06-23 12:08:34 -0300

    transferencia okay

--- src/controller/TransacaoMB.java
+++ src/controller/TransacaoMB.java
@@ -1,5 +1,19 @@
 package controller;
-
+// ExternalContext externalContext =
+// FacesContext.getCurrentInstance().getExternalContext();
+// HttpSession session = (HttpSession) externalContext.getSession(true);
+// Conta c = (Conta)session.getAttribute("conta");
+// Conta c = (Conta)
+// FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("conta");
+// Conta c = (Conta)
+// context.getApplication().evaluateExpressionGet(context,
+// "#{contaMB.conta}", Conta.class);
+// c.setSaldo(c.getSaldo() + transacaoAtual.getValor());
+// ContaDAO.atualizar(c);
+// transacaoAtual.setAgenciaDestino(c.getContaUsuario().getAgencia());
+// transacaoAtual.setContaDestino(c.getContaUsuario().getConta());
+// transacaoAtual.setCpfDestino(c.getUsuario().getCpf());
+// transacaoAtual.setTipo("Depósito");
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -7,8 +21,10 @@ import java.util.Date;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.ValidatorException;
+import javax.servlet.http.HttpSession;
 
 import dao.ContaDAO;
 import dao.TransacaoDAO;
@@ -38,13 +54,13 @@ public class TransacaoMB {
 		this.transacaoAtual = transacaoAtual;
 	}
 
-	public void transferir() {
+	public String transferir() {
 		FacesContext context = FacesContext.getCurrentInstance();
 		Conta c = (Conta) context.getApplication().evaluateExpressionGet(context, "#{contaMB.conta}", Conta.class);
 		transacaoAtual.setContaOrigem(c);
-		if (transacaoAtual.getContaOrigem().equals(c)) {
-			FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_INFO,
-					"Transação inválida", "Não é possível transferir para a própria conta.");
+		if (!transacaoAtual.getContaOrigem().equals(c)) {
+			FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_INFO, "Transação inválida",
+					"Não é possível transferir para a própria conta.");
 
 			throw new ValidatorException(mensagem);
 		} else {
@@ -53,6 +69,7 @@ public class TransacaoMB {
 			if (contaDestino != null) {
 				if (transacaoAtual.getValor() <= c.getSaldo()) {
 					contaDestino.setSaldo(contaDestino.getSaldo() + transacaoAtual.getValor());
+					c.setSaldo(c.getSaldo() - transacaoAtual.getValor());
 					ContaDAO.atualizar(contaDestino);
 					transacaoAtual.setTipo("Transferência");
 					try {
@@ -63,18 +80,20 @@ public class TransacaoMB {
 					}
 					TransacaoDAO.adicionar(transacaoAtual);
 				} else {
-					FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_INFO,
-							"Saldo insuficiente", "Seu saldo não é suficiente para completar a transação");
+					FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_INFO, "Saldo insuficiente",
+							"Seu saldo não é suficiente para completar a transação");
 
 					throw new ValidatorException(mensagem);
 				}
 
 			} else {
-				FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_INFO,
-						"Conta não existe", "A conta informada não existe.");
+				FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_INFO, "Conta não existe",
+						"A conta informada não existe.");
 
-				throw new ValidatorException(mensagem);			}
+				throw new ValidatorException(mensagem);
+			}
 		}
+		return "conta";
 	}
 
 	public String depositar() {
